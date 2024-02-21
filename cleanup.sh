da68975dfe0efe0812e0452c259269e607988190
Thomas Woehrle <thomas.e.woehrle@gmail.com>
2024-02-21 09:14:25 -0600

    Add cleanup_info.txt generation to cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Function to count the number of files recursively in a directory
+count_files() {
+    local dir="$1"
+    local count=$(find "$dir" -type f | wc -l)
+    echo "$count"
+}
+
 # Check for the correct number of arguments
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <input_directory>. Has to be an absolute path."
@@ -8,10 +15,10 @@ fi
 
 # Assign input argument to a variable
 input_directory="$1"
+num_before=$(count_files "$input_directory")
 
 # Loop through all files in the input directory
 for dir in "$input_directory"/*; do
-    # Check if the file is a regular file (not a directory)
     if [ -d "$dir" ]; then
         # Run your Python3 module on the file
         python3 cleanup.py "$dir/left_cam"
@@ -19,3 +26,21 @@ for dir in "$input_directory"/*; do
         # maybe also delete rosbag.csv ?
     fi
 done
+
+info_file="$input_directory/cleanup_info.txt"
+
+# Check if info.txt exists, if not create it
+if [ ! -e "$info_file" ]; then
+    num_after=$(count_files "$input_directory")
+    current_date=$(date +"%Y-%m-%d %H:%M:%S")
+    echo -e "Number of files before (includes /heuristic contents): $num_before 
+Number of files after: $num_after
+    \nHeuristic accuracy of 100% would equal: Number of files after = 0.5 * $num_before 
+Actual Heuristic accuracy approximation: $num_after / (0.5 * $num_before): 
+    \nApproximated accuracy of heuristic: $(echo "$num_after / ($num_before * 0.5)" | bc -l)
+    \nDate of cleanup: $current_date" > "$info_file"
+    echo "Info file created."
+else
+    echo "Info file already exists."
+fi
+
